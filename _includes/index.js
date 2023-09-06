b90f606526cab888ecb206e9cb3c7f6cf96ec028
mpressen <maximilien.pressense@hey.com>
2023-09-06 10:05:10 +0200

    polish file system

--- _includes/index.js
+++ _includes/index.js
@@ -1,4 +1,4 @@
 window.addEventListener('load', () => {
-  {% include /js/index/scrolling_banner_auto.js %}
-  {% include /js/index/value_offer_animation.js %}
+  {% include /js/scrolling_banner_auto.js %}
+  {% include /js/value_offer_animation.js %}
 });
